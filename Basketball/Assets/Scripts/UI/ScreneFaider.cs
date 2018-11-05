4ef40bf12273813101c8978b5d266c682ed91c92
fomich486 <42343682+fomich486@users.noreply.github.com>
2018-11-06 00:05:55 +0200

    All diagonal faiders
    Created all dagonal faiders

--- Basketball/Assets/Scripts/UI/ScreneFaider.cs
+++ Basketball/Assets/Scripts/UI/ScreneFaider.cs
@@ -31,8 +31,14 @@ public class ScreneFaider : MonoBehaviour {
 		//StartCoroutine (FadeRightLeftPrimitiv());
 		//Up - Down
 		//StartCoroutine (FadeUpDownPrimitiv());
-		//Diagonal leftUp- rightDown
-		StartCoroutine (FadeUpLeftToDownRight());
+		//Down - Up
+		//StartCoroutine (FadeDownUpPrimitiv());
+
+		//Diagonal
+		//StartCoroutine (FadeDiagonalUpLeft());
+		//StartCoroutine (FadeDiagonalBottomRight())
+		//StartCoroutine (FadeDiagon;alBottomLeft());
+		StartCoroutine (FadeDiagonalUpRight());
 
 	}
 
@@ -85,57 +91,67 @@ public class ScreneFaider : MonoBehaviour {
 	IEnumerator FadeLeftRightPrimitiv()
 	{
 		RandomColor ();
-		while(tilesList[_numWidth*_numHeight - 1].sizeDelta.x > 0)
-			for (int i = 0; i < _numWidth; i++) {
-				for (int j = 0; j < _numHeight; j++) {
-					RectTransform tile = tilesList [i*_numHeight + j].GetComponent<RectTransform> ();
-				tile.sizeDelta -= delta * Vector2.one;
-				//Like an option but rally whery slow
-				//	yield return null;
-				}
-				yield return new WaitForSeconds(0.05f);
+		for (int i = 0; i < _numWidth; i++) {
+			for (int j = 0; j < _numHeight; j++) {
+				RectTransform tile = tilesList [i*_numHeight + j].GetComponent<RectTransform> ();
+				tile.sizeDelta =  Vector2.zero;;
+			//Like an option but rally whery slow
+			//	yield return null;
 			}
+			yield return new WaitForSeconds(delta);
+		}
 		DestroyList ();
 	}
 	//Change line size from  right to left (whene option yield from up-left to right)
 	IEnumerator FadeRightLeftPrimitiv()
 	{
 		RandomColor ();
-		while(tilesList[_numWidth*_numHeight - 1].sizeDelta.x > 0)
-			for (int i = _numWidth-1; i >= 0; i--) {
-				for (int j = _numHeight-1; j >= 0; j--) {
-					RectTransform tile = tilesList [i*_numHeight + j].GetComponent<RectTransform> ();
-					tile.sizeDelta -= delta * Vector2.one;
-					//Like an option but rally whery slow
-					//	yield return null;
-				}
-				yield return new WaitForSeconds(0.05f);
+		for (int i = _numWidth - 1; i >= 0; i--) {
+			for (int j = _numHeight - 1; j >= 0; j--) {
+				RectTransform tile = tilesList [i * _numHeight + j].GetComponent<RectTransform> ();
+				tile.sizeDelta = Vector2.zero;
+				//Like an option but rally whery slow
+				//	yield return null;
 			}
+			yield return new WaitForSeconds (delta);
+		}
 		DestroyList ();
 	}
 
 	IEnumerator FadeUpDownPrimitiv()
 	{
 		RandomColor ();
-		while(tilesList[_numWidth*_numHeight - 1].sizeDelta.x > 0)
-			for (int i = 0; i < _numHeight; i++) {
-				for (int j = 0; j < _numWidth; j++) {
-					RectTransform tile = tilesList [i + j*_numHeight].GetComponent<RectTransform> ();
-					print (string.Format ("i + j *numHeight = {0}", i + j * _numHeight));
-					tile.sizeDelta -= delta * Vector2.one;
-					//Like an option but rally whery slow
-					//	yield return null;
-				}
-				yield return null;
+		for (int i = 0; i < _numHeight; i++) {
+			for (int j = 0; j < _numWidth; j++) {
+				RectTransform tile = tilesList [i + j * _numHeight].GetComponent<RectTransform> ();
+				print (string.Format ("i + j *numHeight = {0}", i + j * _numHeight));
+				tile.sizeDelta = Vector2.zero;
+				//Like an option but rally whery slow
+				//	yield return null;
 			}
+			yield return null;
+		}
 		DestroyList ();
 	}
 
-	//add Down - Up
+	IEnumerator FadeDownUpPrimitiv()
+	{
+		RandomColor ();
+		for (int i = 0; i < _numHeight; i++) {
+			for (int j = 0; j < _numWidth; j++){
+				RectTransform tile = tilesList [(_numWidth*_numHeight-1)-i - j*_numHeight].GetComponent<RectTransform> ();
+				print (string.Format ("i + j *numHeight = {0}", i + j * _numHeight));
+				tile.sizeDelta =Vector2.zero;
+				//Like an option but rally whery slow
+			}
+			yield return new WaitForSeconds(delta);
+		}
+		DestroyList ();
+	}
 	#endregion
 
 	#region Diagonal
-	IEnumerator FadeUpLeftToDownRight()
+	IEnumerator FadeDiagonalUpLeft()
 	{
 		RandomColor ();
 		int n = (_numWidth + _numHeight) - 1;
@@ -184,15 +200,195 @@ public class ScreneFaider : MonoBehaviour {
 				}
 			}
 
-			yield return new WaitForSeconds(0.05f);
+			yield return new WaitForSeconds(delta);
 		}
+		DestroyList ();
 	}
+
+	IEnumerator FadeDiagonalBottomRight()
+	{
+		RandomColor ();
+		int n = (_numWidth + _numHeight) - 1;
+		int k = 1;
+		int step = _numHeight - 1;
+		int coef = 0;
+		int diffNxM = Mathf.Abs (_numWidth - _numHeight);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k; j++) {
+				print (string.Format ("(i + coef) + j*step = {0}",(i + coef) + j*step));
+				int number = (_numWidth*_numHeight-1) - ((i + coef) + j * step);
+				RectTransform tile = tilesList[number].GetComponent<RectTransform> ();
+
+				tile.sizeDelta = Vector2.zero;
+			}
+			//square
+			if (_numWidth == _numHeight) {
+				if (i < step) {
+					k++;
+
+				} else if (i >= step) {
+					k--;
+					coef += step;
+				}
+			}
+			//not square
+			else if (_numWidth < _numHeight) {
+				if (i < step) {
+					if(i<_numWidth - 1)
+						k++;
+
+				} else if (i >= step) {
+					k--;
+					coef += step;
+				}
+			}
+
+			else if (_numWidth > _numHeight) {
+				if (i < step) {
+					k++;
+
+				} else if (i >= step) {
+					if (i >= step + diffNxM) {
+						k--;
+					}
+					coef += step;
+				}
+			}
+
+			yield return new WaitForSeconds(delta);
+		}
+		DestroyList ();
+	}
+
+	IEnumerator FadeDiagonalBottomLeft()
+	{
+		RandomColor ();
+		int n = (_numWidth + _numHeight) - 1;
+		int k = 1;
+		int step = _numHeight + 1;
+		int coef = 0;
+		int counter =0;
+		int diffNxM = Mathf.Abs (_numWidth - _numHeight);
+		for (int i = 0; i < n; i++) {
+//			print ("k = " + k);
+//			print ("i = " + i);
+			for (int j = 0; j < k; j++) {
+				int number = (_numHeight - 1) - (i - coef) + j * step;
+				print (string.Format ("number = {0}",number));
+				RectTransform tile = tilesList[number].GetComponent<RectTransform> ();
+
+				tile.sizeDelta = Vector2.zero;
+			}
+			//square
+			if (_numWidth == _numHeight) {
+				if (i < _numHeight - 1) {
+					k++;
+
+				} else if (i >=_numHeight - 1) {
+					k--;
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+			//not square
+			else if (_numWidth < _numHeight) {
+				if (i < _numWidth - 1) {
+					if(i<_numWidth - 1)
+						k++;
+
+				} else if (i >= _numHeight - 1) {
+					k--;
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+
+			else if (_numWidth > _numHeight) {
+				if (i <_numHeight - 1) {
+					k++;
+
+				} else if (i >= _numHeight - 1) {
+					if (i >= _numHeight - 1 + diffNxM) {
+						k--;
+					}
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+
+			yield return new WaitForSeconds(delta);
+		}
+		DestroyList ();
+	}
+
+	IEnumerator FadeDiagonalUpRight()
+	{
+		RandomColor ();
+		int n = (_numWidth + _numHeight) - 1;
+		int k = 1;
+		int step = _numHeight + 1;
+		int coef = 0;
+		int counter =0;
+		int diffNxM = Mathf.Abs (_numWidth - _numHeight);
+		for (int i = 0; i < n; i++) {
+			//			print ("k = " + k);
+			//			print ("i = " + i);
+			for (int j = 0; j < k; j++) {
+				int number = ((_numHeight )*(_numWidth-1)) + (i - coef) - j * step;
+				print (string.Format ("number = {0}",number));
+				RectTransform tile = tilesList[number].GetComponent<RectTransform> ();
+
+				tile.sizeDelta = Vector2.zero;
+			}
+			//square
+			if (_numWidth == _numHeight) {
+				if (i < _numHeight - 1) {
+					k++;
+
+				} else if (i >=_numHeight - 1) {
+					k--;
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+			//not square
+			else if (_numWidth < _numHeight) {
+				if (i < _numWidth - 1) {
+					if(i<_numWidth - 1)
+						k++;
+
+				} else if (i >= _numHeight - 1) {
+					k--;
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+
+			else if (_numWidth > _numHeight) {
+				if (i <_numHeight - 1) {
+					k++;
+
+				} else if (i >= _numHeight - 1) {
+					if (i >= _numHeight - 1 + diffNxM) {
+						k--;
+					}
+					coef = i+1 +(1+counter*_numHeight);
+					counter++;
+				}
+			}
+
+			yield return new WaitForSeconds(delta);
+		}
+		DestroyList ();
+	}
+
 
 	#endregion
 	void RandomColor()
 	{
 		foreach (RectTransform tile in tilesList) {
-			Color col = new Color (1f,0f,Random.Range (0f, 1f) );
+			float rand = Random.Range (0f, 1f);
+			Color col = new Color (rand,rand,rand);
 			tile.GetComponent<Image> ().color = col;
 		}
 	}
